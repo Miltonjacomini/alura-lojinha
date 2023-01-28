5e3552ebc47d02a7925af5b41516bb1138399328
Milton Giacomini Neto <milton.jneto@mercadolivre.com>
2023-01-28 13:57:39 -0300

    Adiciona feature de Insert
    No modulo 3 é adicionada a funcionalidade de
    INSERT, novo template foi adicionado e com ele
    seus respectivas funções no controllers e models.

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"log"
 	"miltonjacomini/alura-loja/models"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -10,4 +12,33 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "Index", models.BuscaTodosProdutos())
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvetido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro ao converter preço: ", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro ao converter quantidade: ", err)
+		}
+
+		models.CriaNovoProduto(nome, descricao, precoConvetido, quantidadeConvertida)
+	}
+
+	http.Redirect(w, r, "/", 301)
 }
