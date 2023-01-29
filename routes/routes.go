2481ad21dd5f435fd875c23273ce1d457627a1bf
Milton Giacomini Neto <milton.jneto@mercadolivre.com>
2023-01-29 11:07:50 -0300

    Adiciona função de edição do produto

--- routes/routes.go
+++ routes/routes.go
@@ -10,9 +10,6 @@ func CarregaRotas() {
 	http.HandleFunc("/new", controllers.New)
 	http.HandleFunc("/insert", controllers.Insert)
 	http.HandleFunc("/delete", controllers.Delete)
-<<<<<<< HEAD
-=======
 	http.HandleFunc("/edit", controllers.Edit)
 	http.HandleFunc("/update", controllers.Update)
->>>>>>> 77c1291 (Adiciona função remover produto)
 }
